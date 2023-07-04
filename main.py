bec559ca9be72015cb5b8f911d94670b9ab74693
Sergio Escudero Tabares <sergioset15@gmail.com>
2023-07-04 10:10:15 -0500

    Modificación de sintaxis y parametros

--- main.py
+++ main.py
@@ -7,79 +7,87 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import HistGradientBoostingClassifier
 
 # Paso 1: Leer el archivo stroke.csv
-data = pd.read_csv('stroke.csv')
+datos = pd.read_csv('stroke.csv')
 
 # Paso 2: Seleccionar aleatoriamente el 80% del conjunto de datos para entrenar y el 20% restante para las pruebas
-X = data.drop('stroke', axis=1)
-y = data['stroke']
+X = datos.drop('stroke', axis=1)
+y = datos['stroke']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # 80% entrenamiento, 20% pruebas
 
-# Paso 3: Preprocesamiento de variables categóricas y numéricas
-categorical_features = ['gender']  # Reemplaza con las columnas categóricas presentes en tus datos
-numeric_features = ['age', 'bmi', 'avg_glucose_level']  # Reemplaza con las columnas numéricas presentes en tus datos
+# Paso 3: Utilizar una estrategia para normalizar los datos y llenar los datos faltantes
+categoricas = ['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status']
+numericas = ['age', 'hypertension', 'heart_disease', 'avg_glucose_level', 'bmi']
 
 # Codificación one-hot para variables categóricas y escalado para variables numéricas
-preprocessor = ColumnTransformer(
+modificacionesPreProcesado = ColumnTransformer(
     transformers=[
-        ('cat', OneHotEncoder(), categorical_features),
-        ('num', StandardScaler(), numeric_features)
+        ('cat', OneHotEncoder(), categoricas),
+        ('num', StandardScaler(), numericas)
     ])
 
-X_train_processed = preprocessor.fit_transform(X_train)
-X_test_processed = preprocessor.transform(X_test)
+X_train_processed = modificacionesPreProcesado.fit_transform(X_train)
+X_test_processed = modificacionesPreProcesado.transform(X_test)
 
-# Paso 4: Configurar los hiperparámetros y obtener los árboles de decisión
-hyperparameters = range(5, 51, 5)
-decision_trees = []
-for max_depth in hyperparameters:
+# Paso 4: Configurar los hiperparámetros del árbol de decisión de la siguiente manera: criterion=gini, splitter=best,
+# y random_state=123. Obtener 10 árboles de decisión que resultan de modificar el hiperparámetro
+# max_depth desde 5 hasta 50 con incrementos de 5
+
+hiperparametro = range(5, 51, 5)
+
+arbolesDecisionPaso4 = []
+for max_depth in hiperparametro:
     classifier = DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=max_depth, random_state=123)
     classifier.fit(X_train_processed, y_train)
-    decision_trees.append(classifier)
+    arbolesDecisionPaso4.append(classifier)
 
-# Paso 5: Generar tabla de accuracy para los árboles de decisión
+# Paso 5: Incluya en el notebook una tabla con el accuracy para los 10 árboles del punto anterior
 
-# Crear una lista para almacenar los resultados de accuracy
-accuracy_scores = []
+valoresPrecisionPaso4 = []
+for max_depth, tree in zip(hiperparametro, arbolesDecisionPaso4):
+    precision = tree.score(X_test_processed, y_test)
+    valoresPrecisionPaso4.append(precision)
 
-# Calcular y almacenar el accuracy de cada árbol de decisión
-for max_depth, tree in zip(hyperparameters, decision_trees):
-    accuracy = tree.score(X_test_processed, y_test)
-    accuracy_scores.append(accuracy)
+tablaPaso4 = pd.DataFrame({'Profundidad máxima': hiperparametro, 'Precisión': valoresPrecisionPaso4})
+print('Tabla de precisión de arboles punto 4')
+print(tablaPaso4, "\n")
 
-# Crear un DataFrame con los resultados
-results_df = pd.DataFrame({'Max Depth': hyperparameters, 'Accuracy': accuracy_scores})
+# Paso 6: Repita el mismo procedimiento del punto 4 usando como hiperparámetros criterion=entropy, splitter=best,
+# random_state=123, y variando el hiperparámetro max_depth desde 5 hasta 50 con incrementos de 5
 
-# Mostrar la tabla con los resultados
-print(results_df)
-
-# Paso 6: Configurar los hiperparámetros y obtener los árboles de decisión con criterion=entropy
-decision_trees_entropy = []
-for max_depth in hyperparameters:
+arbolesDecisionPaso6 = []
+for max_depth in hiperparametro:
     classifier = DecisionTreeClassifier(criterion='entropy', splitter='best', max_depth=max_depth, random_state=123)
     classifier.fit(X_train_processed, y_train)
-    decision_trees_entropy.append(classifier)
+    arbolesDecisionPaso6.append(classifier)
 
-# Paso 7: Calcular y mostrar la tabla con los resultados de accuracy para los árboles de decisión con criterion=entropy
-accuracy_scores_entropy = []
-for max_depth, tree in zip(hyperparameters, decision_trees_entropy):
+# Paso 7: Incluya en el notebook una tabla con el accuracy para los 10 árboles del punto anterior
+
+valoresPrecisionPaso6 = []
+for max_depth, tree in zip(hiperparametro, arbolesDecisionPaso6):
     accuracy = tree.score(X_test_processed, y_test)
-    accuracy_scores_entropy.append(accuracy)
+    valoresPrecisionPaso6.append(accuracy)
 
-results_df_entropy = pd.DataFrame({'Max Depth': hyperparameters, 'Accuracy': accuracy_scores_entropy})
-print(results_df_entropy)
+tablaPaso6 = pd.DataFrame({'Profundidad máxima': hiperparametro, 'Precisión': valoresPrecisionPaso6})
+print('Tabla de precisión de arboles punto 6')
+print(tablaPaso6, "\n")
 
-# Paso 8: Configurar los hiperparámetros y obtener los árboles de decisión con criterion=entropy y splitter=random
-decision_trees_random = []
-for max_depth in hyperparameters:
+# Paso 8: Repita el mismo procedimiento del punto 4 usando como hiperparámetros criterion=entropy,
+# splitter=random, random_state=123, y variando el hiperparámetro max_depth desde 5 hasta 50 con
+# incrementos de 5
+
+arbolesDecisionPaso8 = []
+for max_depth in hiperparametro:
     classifier = HistGradientBoostingClassifier(max_depth=max_depth, random_state=123)
     classifier.fit(X_train_processed, y_train)
-    decision_trees_random.append(classifier)
+    arbolesDecisionPaso8.append(classifier)
 
 # Paso 9: Calcular y mostrar la tabla con los resultados de accuracy para los árboles de decisión con criterion=entropy y splitter=random
-accuracy_scores_random = []
-for max_depth, tree in zip(hyperparameters, decision_trees_random):
-    accuracy = tree.score(X_test_processed, y_test)
-    accuracy_scores_random.append(accuracy)
 
-results_df_random = pd.DataFrame({'Max Depth': hyperparameters, 'Accuracy': accuracy_scores_random})
-print(results_df_random)
+valoresPrecisionPaso8 = []
+for max_depth, tree in zip(hiperparametro, arbolesDecisionPaso8):
+    accuracy = tree.score(X_test_processed, y_test)
+    valoresPrecisionPaso8.append(accuracy)
+
+tablaPaso8 = pd.DataFrame({'Profundidad máxima': hiperparametro, 'Precisión': valoresPrecisionPaso8})
+print('Tabla de precisión de arboles punto 8')
+print(tablaPaso8, "\n")
